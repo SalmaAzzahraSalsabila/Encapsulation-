852796a7aac2c56bfb2bee425fd462f2f7e9a51a
Salma Azzahra Salsabila <salma.2000@students.amikom.ac.id>
2019-04-13 09:03:22 +0700

    encapsulation

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat instan class / object
+            Time objTime = new Time();
+
+            // mengambil nilai dengan property
+            Console.WriteLine("Year : {0}", objTime.Year.ToString());
+            Console.WriteLine("Month : {0}", objTime.Month.ToString());
+            Console.WriteLine("Date : {0}", objTime.Date);
+            Console.WriteLine("Hour : {0}", objTime.Hour);
+            Console.WriteLine("Minute : {0}", objTime.Minute);
+            Console.WriteLine("Second : {0}", objTime.Second);
+
+            //memberi nilai property
+            objTime.Year = 2009;
+            objTime.Month = 5;
+            objTime.Date = 1;
+
+            //mengambil nilai dengan property
+            Console.WriteLine("Year : {0}", objTime.Year.ToString());
+            Console.WriteLine("Month : {0}", objTime.Month.ToString());
+            Console.WriteLine("Date : {0}", objTime.Date);
+
+            // memanggil method pada class tersebut
+            objTime.DisplayCurrentTime();
+
+            // bisa diakses karena access modifier public
+            //objTime.year = 2010;
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClass
+{
+    class Time
+    {
+        // private variables atau field
+        private int year;
+        private int month;
+        private int date;
+        private int hour;
+        private int minute;
+        private int second;
+
+        // constructor
+        public Time()
+        {
+            year = 2018;
+            month = 10;
+            date = 10;
+            hour = 10;
+            minute = 30;
+            second = 40;
+        }
+
+        // property
+        public int Year {
+            get { return year; }
+            set { year = value; }
+        }
+        public int Month {
+            get { return month; }
+            set { month = value; }
+        }
+        public int Date {
+            get { return date; }
+            set { date = value; }
+        }
+        public int Hour {
+            get { return hour; }
+            set { hour = value; }
+        }
+        public int Minute {
+            get { return minute; }
+            set { minute = value; }
+        }
+        public int Second {
+            get { return second; }
+            set { second = value; }
+        }
+
+        // public methods
+        public void DisplayCurrentTime()
+        {
+            Console.WriteLine("Waktu sekarang : {0}", DateTime.Now.ToString());
+        }
+    }
+}
